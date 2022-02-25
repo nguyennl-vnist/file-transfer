f901ba61e181ee7093a82f3e1564fd74a7af6363
Luong Nguyen <nguyennl.vnist@gmail.com>
2022-02-25 13:39:17 +0700

    finish

--- client/src/landingPage/header.jsx
+++ client/src/landingPage/header.jsx
@@ -2,7 +2,7 @@ import BasicTabs from '../basicTab';
 
 export const Header = (props) => {
   return (
-    <div class='htmlContents' style={{ paddingTop: 100, height: "auto" }}>
+    <div class='htmlContents' style={{ paddingTop: 100, height: "auto", minHeight: "550px" }}>
       <BasicTabs />
     </div>
   )
